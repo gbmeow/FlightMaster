2dd2300bd38eb55a6b736d92768136c89062b280
Jerzy Batalinski <george.batalinski@gmail.com>
2015-06-26 09:19:48 -0400

    add exercise 11 backend and ctrl

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,11 @@
 'use strict';
 
 angular.module('flightMasterApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.listOfExercises = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/exercises').success(function(exercisesList) {
+      $scope.listOfExercises = exercisesList;
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
   });
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,9 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/exercises/exercises.socket').register(socket);
+  require('../api/tennis/tennis.socket').register(socket);
+  require('../api/message/message.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
